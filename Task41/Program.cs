7431372431d0c3261545dd46cff3a36620a3130d
«Dmitriy <vorotov87@mail.ru>
2022-05-16 19:13:48 +1000

    Task41

---
+++ Task41/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел.
+// Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+
+int[] mass = new int[10];
+int count = 0;
+ 
+for (int i = 0; i < mass.Length; i++)
+{
+    mass[i] = new Random().Next(-100, 100);
+}
+
+for (int i = 0; i < mass.Length; i++)
+{
+    Console.Write(mass[i] + " ");
+}
+Console.WriteLine();
+for (int i = 0; i < mass.Length; i++)
+{
+    if (mass[i] > 0)
+    {
+        count++;
+    }
+}
+ 
+Console.WriteLine($"Кол-во элементов > 0: {count}");
